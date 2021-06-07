6bca4a03984f80cf3b5aa9f977407bf863690041
Refik Gulak <74262442+Refik48@users.noreply.github.com>
2021-06-07 16:35:29 +0100

    07.06

--- ornek2_komponent/src/index.js
+++ ornek2_komponent/src/index.js
@@ -1,17 +1,16 @@
-//React'in temel kutuphaneleri
+// React'ın temel kutuphaneleri
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-//App modulunun eklenmesi
+// App modulunun eklenmesi
 import App from "./App";
-// Web sayfasina basilacak olan herseyi olusturan veya guncelleyen metot.(render)
-// ? render method’u 2 parametre alir
+// Web sayfasina basilacak olan herseyi olusturan veya guncelleyen metot.
+// ! Web sayfasina basilacak olan herseyi olusturan veya guncelleyen method
+// ? render method'u 2 parametre alir
 // ** 1. parametre hangi modulu render edecegidir.
 // ** 2. parametre nerede render edilecegidir.
 ReactDOM.render(
   <React.StrictMode>
-    <App /> {/* App modulunun cagrilmasi  */}
-    <App /> {/* App modulunun cagrilmasi  */}
+    <App /> {/* App modulunun cagrilmasi */}
   </React.StrictMode>,
-  document.getElementById("root") //** .2.kisim
+  document.getElementById("root")
 );
